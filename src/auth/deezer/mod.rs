c0a4d7ac68795b2bfbb176c10c2617c55bbd1745
Jiří Konečný <DragonLichcz@gmail.com>
2015-08-01 23:29:37 +0200

    Add license string to sources

--- src/auth/deezer/mod.rs
+++ src/auth/deezer/mod.rs
@@ -1,3 +1,18 @@
+// This file is part of libmusic_streamer.
+//
+// libmusic_streamer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// libmusic_streamer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libmusic_streamer.  If not, see <http://www.gnu.org/licenses/>.
+
 //! Specific deezer implementation of authorization and authentication trait.
 //! Object AuthObjectDeezer will implement whole process.
 
